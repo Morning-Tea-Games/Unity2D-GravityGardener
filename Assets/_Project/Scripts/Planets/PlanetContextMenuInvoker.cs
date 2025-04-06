a96128bb463b279e882ed21dd86aaa69d93804ab
Alexandr Gyach <as.gyach@gmail.com>
2025-04-06 13:24:55 +0300

    fix: added dual side composit

--- Assets/_Project/Scripts/Planets/PlanetContextMenuInvoker.cs
+++ Assets/_Project/Scripts/Planets/PlanetContextMenuInvoker.cs
@@ -16,7 +16,7 @@ namespace Planets
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-            
+
             if (hit.collider != null && hit.collider.TryGetComponent<Planet>(out Planet planet))
             {
                 return planet;
@@ -28,20 +28,18 @@ namespace Planets
         private void Update()
         {
             if (EventSystem.current.IsPointerOverGameObject())
-            {
                 return;
-            }
 
             if (Input.GetMouseButtonDown(1))
             {
                 Planet clickedPlanet = GetClickedPlanet();
-                
+
                 if (clickedPlanet == _main)
                 {
                     _menu.SetTarget(_main);
                     _contextMenuObject.transform.position = _main.transform.position;
                     _contextMenuObject.SetActive(true);
-
+                    
                     if (PlanetBuffer.Instance.PlanetA == null && !_menu.WaitingConnect)
                     {
                         PlanetBuffer.Instance.SetA(_main);
@@ -53,9 +51,12 @@ namespace Planets
             {
                 Planet clickedPlanet = GetClickedPlanet();
 
-                if (clickedPlanet == _main && _menu.WaitingConnect && PlanetBuffer.Instance.PlanetA != null)
+                if (_menu.WaitingConnect &&
+                    PlanetBuffer.Instance.PlanetA != null &&
+                    clickedPlanet != null &&
+                    clickedPlanet != PlanetBuffer.Instance.PlanetA)
                 {
-                    PlanetBuffer.Instance.SetB(_main);
+                    PlanetBuffer.Instance.SetB(clickedPlanet);
                     _connector.Connect();
                     _menu.WaitingConnect = false;
                     _menu.SetActiveCompositButton(true);
